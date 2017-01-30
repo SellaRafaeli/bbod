b87e4cd4c9293723ba7bee1be1f525ebb804df7e
Sella Rafaeli <sella.rafaeli@gmail.com>
2017-01-30 21:34:29 +0200

    auto-message.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # bbod
 Big Book of Front-End Development
+
+List of topics
+0-Intro  
+  - Intro
+1-HTML
+  - hello world
+  - html tags 
+  - special tags 
+  - w3schools.
+
+Topics to add:
+- jsbin
--- build.rb
+++ build.rb
@@ -10,14 +10,19 @@ require 'erb'
 
 # filewatcher '**/*.md' 'ruby build.rb; echo "Updated HTMLs"; date'
 
-#2. Find all Markdown files
-md_file_paths = Find.find('.').to_a.select {|f| f =~ /.*\.md$/ }
+# delete all .html
+# Find.find(Dir.pwd) do |path|
+#   File.delete(path) if path =~ /.*\.html$/
+# end
 
-#3. Turn each of them into an HTML file with same name in same place. 
-md_file_paths.each do |path|
-  html_path = path.sub('.md','.html')
-  puts "generating #{path} into #{html_path}..."
-  text = File.read(path)
-  File.write(html_path, Kramdown::Document.new(text, input: 'GFM', coderay_line_numbers: nil).to_html)      
-  #wrap_file_in_html(html_path)    
-end
+#2. Find all Markdown files
+# md_file_paths = Find.find('.').to_a.select {|f| f =~ /.*\.md$/ }
+
+# #3. Turn each of them into an HTML file with same name in same place. 
+# md_file_paths.each do |path|
+#   html_path = path.sub('.md','.html')
+#   puts "generating #{path} into #{html_path}..."
+#   text = File.read(path)
+#   File.write(html_path, Kramdown::Document.new(text, input: 'GFM', coderay_line_numbers: nil).to_html)      
+#   #wrap_file_in_html(html_path)    
+# end
